5d5df0c0c02bd3d344222a3a02b96931caac6442
limeng1900 <276159433@qq.com>
2017-09-26 22:22:47 +0800

    suffixFilter

--- file/suffixFilter.py
+++ file/suffixFilter.py
@@ -1,1 +1,33 @@
-#按照文件名过滤文件
+# -*- coding:utf-8 -*-
+
+#以后缀名过滤文件,可同时过滤多个后缀名
+#input：suffixFilter(文件夹路径,*一个或多个后缀名)  eg.suffixFilter('./result','.xlsx','.txt')
+#output：(符合该后缀的文件路径数组, 符合该后缀的文件名数组)
+
+import os
+
+def suffixFilter(folderName,*endstring):
+    if not map(folderName.endswith, '/'):
+        folderName = folderName+'/'
+    listFile = os.listdir(folderName)
+    a = endWith(*endstring)
+    f_file = filter(a, listFile)
+    fileSuffixPath=[]
+    fileSuffixName=[]
+    for file in f_file:
+        filePath = folderName + file
+        fileSuffixPath.append(filePath)
+        fileSuffixName.append(file)
+    return (fileSuffixPath,fileSuffixName)
+
+def endWith(*endstring):
+    ends = endstring
+    def run(s):
+        f=map(s.endswith,ends)
+        if True in f:return s
+    return run
+
+if __name__=='__main__':
+    fileSuffixPath, fileSuffixName = suffixFilter('./result','.xlsx','.txt')
+    print(fileSuffixPath)
+    print(fileSuffixName)
